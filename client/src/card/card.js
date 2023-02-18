f7704e4c80b8ced6f3c42755f3f61f17717f867e
Bilal Malik <bilalmalik504@yahoo.com>
2023-02-18 23:30:16 +0530

    added card to homepage

---
+++ client/src/card/card.js
@@ -0,0 +1,77 @@
+import './card.css';
+import doggo from '../assets/dog.png';
+import doggo_2 from '../assets/doggo_pic.png';
+import firstaid from '../assets/first-aid-kit.png';
+import lovepng from '../assets/love.png';
+import netpng from '../assets/net.png';
+import salarypng from '../assets/salary.png';
+const Card = () => {
+  return (
+    <div className="wrapper">
+    <div className="outer">
+      <div className="card card-1">
+        <div className="content">
+          <div className="img"><img src={doggo} alt=""/></div>
+          <div className="details">
+            <span className="name">850+</span>
+            <p>Saved Animals</p>
+          </div>
+        </div>
+        {/* <!-- <a href="#">Follow</a> --> */}
+      </div>
+      <div class="card card0" >
+        <div class="content">
+          <div class="img"><img src={firstaid} alt=""/></div>
+          <div class="details">
+            <span class="name">700+</span>
+            <p>Animals spayed & neutered</p>
+          </div>
+        </div>
+        {/* <!-- <a href="#">Follow</a> --> */}
+      </div>
+      <div className="card card1" >
+        <div className="content">
+          <div className="img"><img src={lovepng} alt=""/></div>
+          <div className="details">
+            <span className="name">75</span>
+            <p>dogs adopted, 310 released</p>
+          </div>
+        </div>
+         {/* <a href="#">Follow</a>  */}
+      </div>
+      {/* <!-- <div class="card" style="--delay:2;">
+        <div class="content">
+          <div class="img"><img src="#" alt=""/></div>
+          <div class="details">
+            <span class="name">Justin Chung</span>
+            <p>Backend Developer</p>
+          </div>
+        </div>
+        <a href="#">Follow</a>
+      </div> --> */}
+      <div className="card card2">
+        <div className="content">
+          <div className="img"><img src={netpng} alt=""/></div>
+          <div className="details">
+            <span className="name">98</span>
+            <p>Animal abusers counseled</p>
+          </div>
+        </div>
+         {/* <a href="#">Follow</a>  */}
+      </div>
+      <div className="card card2" >
+        <div className="content">
+          <div className="img"><img src={salarypng} alt=""/></div>
+          <div className="details">
+            <span className="name">Rs 1.8L</span>
+            <p>of funding granted to 11 local shelters</p>
+          </div>
+        </div>
+         {/* <a href="#">Follow</a> */}
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default Card;
--- client/src/homepage/home.js
+++ client/src/homepage/home.js
@@ -1,11 +1,13 @@
 import './home.css';
 import Landing from '../landingscreen/landing';
 import Stats from '../stats/stats';
+import Card from '../card/card';
 const Home = () => {
   return (
     <div className="homepage">
       <Landing/>
       <Stats/>
+      <Card/>
     </div>
   );
 }
