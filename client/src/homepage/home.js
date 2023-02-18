f25a8bd42ac3b258deb0446c963e56893b15a8ce
Bilal Malik <bilalmalik504@yahoo.com>
2023-02-19 00:15:19 +0530

    added card and card2

--- client/src/homepage/home.js
+++ client/src/homepage/home.js
@@ -2,12 +2,17 @@ import './home.css';
 import Landing from '../landingscreen/landing';
 import Stats from '../stats/stats';
 import Card from '../card/card';
+// import Slider from '../slider/slider';
+import Cardtwo from '../card2/cardtwo';
 const Home = () => {
   return (
     <div className="homepage">
       <Landing/>
       <Stats/>
       <Card/>
+      <Cardtwo/>
+      {/* <Slider/> */}
+
     </div>
   );
 }
